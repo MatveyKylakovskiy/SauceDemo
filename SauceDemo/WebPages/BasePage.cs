ee3899edca4987c7396fc0f3db0175a2d7324119
matveykylak <matveykylak@gmail.com>
2024-06-12 13:19:25 +0300

    Add project files.

---
+++ SauceDemo/WebPages/BasePage.cs
@@ -0,0 +1,13 @@
+﻿using PageObjLib.Factories;
+using PageObjLib.Pages;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+
+namespace SauceDemo.WebPages
+{
+    internal static class BasePage
+    {
+        private const string _url = "";
+
+    }
+}
---
+++ SauceDemo/WebPages/LoginPage.cs
@@ -0,0 +1,18 @@
+﻿using PageObjLib.Factories;
+using PageObjLib.Pages;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+
+namespace SauceDemo.WebPages
+{
+    internal static class LoginPage
+    {
+        private const string _url = "https://www.saucedemo.com";
+
+        private static IWebElement UserNameInput() => Page.GetElement(By.Id("user-name"));
+        private static IWebElement PasswordInput() => Page.GetElement(By.Name("password"));
+        private static IWebElement LoginButton() => Page.GetElement(By.XPath("//input[@id='login-button']"));
+        private static IWebElement LoginCredentials() => Page.GetElement(By.ClassName("login_credentials_wrap"));
+
+    }
+}
